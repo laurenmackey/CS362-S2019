7f80c875b253e4b9caedb90e600e6582d8576556
laurenmackey <lauren.m.mackey@gmail.com>
2019-04-28 10:05:18 -0700

    Add more work to unit test 1

--- projects/mackeyl/dominion/dominion_helpers.h
+++ projects/mackeyl/dominion/dominion_helpers.h
@@ -12,4 +12,6 @@ int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
 
+int playAdventurer(struct gameState *state, int currentPlayer);
+
 #endif
--- projects/mackeyl/dominion/unittest1.c
+++ projects/mackeyl/dominion/unittest1.c
@@ -19,23 +19,73 @@ int main()
 {
     // declare variables
     int seed = 1000;
-    int numPlayer = 2;
+    int numPlayers = 2;
     int cards[10] = {adventurer, gardens, embargo, village, minion, mine, cutpurse,
-           sea_hag, tribute, smithy};
+           sea_hag, estate, smithy};
     struct gameState state;
+    int deckSize = 10;
+    int i, j, k, l, m, n, o;
+    int numTreasurePre[2] = {0, 0};
+    int numTreasurePost[2] = {0, 0};
 
     // initialize the game
     memset(&state, 23, sizeof(struct gameState));
-    game = initializeGame(numPlayer, cards, seed, &state);
+    initializeGame(numPlayers, cards, seed, &state);
 
     // manually set the deck to have: no treasure, 2 treasure, 3 treasure
-    // to do: write a function like the deck creation part of initialize game that sets 
-    // deck to have random ordering of non-treasure, and then some treasure depending on scenario
+    for (i = 0; i < numPlayers; i++)
+    {
+        state.deckCount[i] = 0;
+        for (j = 0; j < 3; j++)
+        {
+            state.deck[i][j] = estate;
+            state.deckCount[i]++;
+        }
+        for (j = 3; j < deckSize; j++)
+        {
+            state.deck[i][j] = minion;
+            state.deckCount[i]++;    
+        }
+    }
 
-    // get number of treasure cards in hand
+    // get number of treasure cards in hand before play
+    for (k = 0; k < numPlayers; k++) 
+    {
+        for (l = 0; l < deckSize; l++)
+        {
+            if (state.deck[k][l] == copper || state.deck[k][l] == silver || state.deck[k][l] == gold)
+            {
+                numTreasurePre[k]++;
+            }
+        } 
+    }
+
+    // play the Adventurer card for both players
+    for (m = 0; m < numPlayers; m++) 
+    {
+        printf("About to play adventurer\n");
+        playAdventurer(&state, m);
+        printf("Played adventurer\n");
+        // to do: run gdb to figure out where error is from
+        // could also write a function to print the state of the game at any time
+    }
 
     // at end, hand should have +2 treasure cards
+    for (n = 0; n < numPlayers; n++) 
+    {
+        for (o = 0; o < deckSize; o++)
+        {
+            if (state.deck[n][o] == copper || state.deck[n][o] == silver || state.deck[n][o] == gold)
+            {
+                numTreasurePost[n]++;
+            }
+        } 
+    }
+
+    printf("Post treasure for player 1 is: %d\n Post treasure for player 2 is: %d\n", numTreasurePost[0], numTreasurePost[1]);
 
     // at end, anything before treasure cards should be in discard pile
+
+    // do positive test for this (just make sure 2 treasures works), then boundary test for the next (no treasure, 10 treasure)
 
 }
