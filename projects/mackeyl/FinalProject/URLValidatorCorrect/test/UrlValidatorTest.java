511aec652e1062f89def2f5eb06a03998ef1e61c
BartHough <39164910+BartHough@users.noreply.github.com>
2019-06-04 12:58:54 -0600

    Fixing out of bounds array access

--- projects/mackeyl/FinalProject/URLValidatorCorrect/test/UrlValidatorTest.java
+++ projects/mackeyl/FinalProject/URLValidatorCorrect/test/UrlValidatorTest.java
@@ -375,7 +375,7 @@ protected void setUp() {
     static boolean incrementTestPartsIndex(int[] testPartsIndex, Object[] testParts) {
       boolean carry = true;  //add 1 to lowest order part.
       boolean maxIndex = true;
-      for (int testPartsIndexIndex = testPartsIndex.length; testPartsIndexIndex >= 0; --testPartsIndexIndex) {
+      for (int testPartsIndexIndex = testPartsIndex.length -1; testPartsIndexIndex >= 0; --testPartsIndexIndex) {
           int index = testPartsIndex[testPartsIndexIndex];
          ResultPair[] part = (ResultPair[]) testParts[testPartsIndexIndex];
          maxIndex &= (index == (part.length - 1));
